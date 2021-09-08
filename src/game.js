4ecde625c59ff48921ba7880e47a5f4473c85232
Guy Balaam <guy.balaam@gmail.com>
2021-09-08 17:42:24 +0100

    Cleaning up logic and commenting

--- src/game.js
+++ src/game.js
@@ -1,37 +1,43 @@
+// Initial constants
 const WIDTH = 800;
 const HEIGHT = 400;
 const RADIUS = 100;
 
+// Setup <canvas/> element
 const content = document.getElementById("content");
-
 const canvas = document.createElement('canvas');
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 content.appendChild(canvas);
+const ctx = canvas.getContext('2d');
 
-const context = canvas.getContext('2d');
-
-function drawBackground(ctx) {
+// Helper functions for drawing
+function drawBackground() {
     ctx.fillStyle = 'indianred';
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
-
-function drawCircle(ctx, x, y, r) {
+function drawCircle(x, y, r) {
     ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI)
     ctx.fill();
 }
+function drawRect(x, y, w, h) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x, y, w, h);
+}
 
+// Main animation loop
 function drawLoop(time) {
-    draw(context, time);
+    draw(time);
     requestAnimationFrame(drawLoop);
 }
 
-function draw(ctx, t) {
-    drawBackground(ctx);
+// Drawing function called every frame
+function draw(t) {
+    // This is where we can put game logic
+    drawBackground();
     drawCircle(
-        ctx,
         WIDTH / 2 + RADIUS * Math.cos(t / 100),
         HEIGHT / 2 + RADIUS * Math.sin(t / 100),
         10
